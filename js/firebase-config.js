69a2abf7876459dd083c987f770322b6838464ea
zawzaw <boexaw@gmail.com>
2026-01-12 16:06:51 +0630

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA1tAW0DWW633uYOZ1cHw-kfnHL2kQrlNM",
   authDomain: "gobike-14050.firebaseapp.com",
@@ -6,7 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "1079730742000",
   appId: "1:1079730742000:web:2ba453b89870404fcc1835"
 };
-firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
